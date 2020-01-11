65e15b2d7b0e1839da95cac220b85616d3718dd1
Michael Clement <mclement@tushka.k12.ok.us>
2020-01-11 15:52:49 -0600

    Edited config.py

---
+++ setup-git-v1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "Michael Clement"
+git config --global user.email "mclement@tushka.k12.ok.us"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
